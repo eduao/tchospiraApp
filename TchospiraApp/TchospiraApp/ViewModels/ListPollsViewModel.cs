96a909b7ebd07bbe833a2d33423e9062e55f0582
Eduardo A. O <emaildoedu00@gmail.com>
2017-06-06 20:29:44 -0300

    Tentativa falha... erros diversos após tentar inserir o Microsoft.Azure.Mobile.Client e atualizar pacotes nuget do xamarin

--- TchospiraApp/TchospiraApp/ViewModels/ListPollsViewModel.cs
+++ TchospiraApp/TchospiraApp/ViewModels/ListPollsViewModel.cs
@@ -1,9 +1,34 @@
-﻿namespace TchospiraApp.ViewModels
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TchospiraApp.Models;
+
+namespace TchospiraApp.ViewModels
 {
     class ListPollsViewModel
     {
+
+        private IMobileServiceClient _client;
+        private IMobileServiceTable<Survey> _surveyTable;
+        private const string Url = "http://tchospira.azurewebsites.net"; // aqui ate onde sei seria o endereço da tabela/
+        
+        public async Task<IEnumerable<Survey>> GetDados()
+        {
+            try
+            {
+                return await _surveyTable.ToEnumerableAsync();
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
         public ListPollsViewModel()
         {
+
+            _client = new MobileServiceClient(Url);
+            _surveyTable = _client.GetTable<Survey>();
 
         }
     }
